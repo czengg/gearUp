68075269a26ac8e37ecc419b0f9fa8efff59aea6
Jay Chopra <jchopra@andrew.cmu.edu>
2014-09-02 16:40:27 -0400

    updated models for match, rank and sport

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,2 +1,14 @@
 class Sport < ActiveRecord::Base
+
+	# RELATIONSHIPS
+	has_many :ranks
+	has_many :matches
+	has_many :players, :through => :ranks
+
+	# SCOPES
+	scope :alphabetical, -> { order("name") }
+
+	# VALIDATIONS
+	validates_presence_of :name
+
 end
